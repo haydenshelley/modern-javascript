627bcc605d036ec1d82404e9107c84e0f9e83571
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-29 10:38:20 -0700

    webpack

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const dayjs = require("dayjs");
+
 console.log("My Birthday");
 console.log(
   dayjs()
@@ -5,5 +7,15 @@ console.log(
     .add(4, "month")
     .add(11, "day")
     .set("year", 1994)
+    .format("MM-DD-YYYY")
+);
+
+console.log("Today's Date");
+console.log(
+  dayjs()
+    .startOf("year")
+    .add(7, "month")
+    .add(28, "day")
+    .set("year", 2023)
     .format("MM-DD-YYYY")
 );
